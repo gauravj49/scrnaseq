8dfe3c530bc57771ae5ed2160a413be556180a0b
Gaurav Jain <gauravj49@gmail.com>
2019-08-29 14:59:02 +0200

    scTransform normalization code added

--- 01.1.2_ashish_manac_imputation_normalization_usage.sh
+++ 01.1.2_ashish_manac_imputation_normalization_usage.sh
@@ -60,101 +60,30 @@ rawMat    <- data.frame(read.table(rawinfile, header=TRUE, sep="\t", na.strings=
 dcaMat    <- data.frame(read.table(dcainfile, header=TRUE, sep="\t", na.strings=c(NA, NaN, Inf), row.names=1))
 
 # Normalize using scTransform
-normRaw_data <- sctransform::vst(as.matrix(rawMat[complete.cases(rawMat * 0), , drop=FALSE]))
-normDCA_data <- sctransform::vst(as.matrix(dcaMat))
+# Normalized data is normData$y
+normRawData <- sctransform::vst(as.matrix(rawMat), return_gene_attr = FALSE)
+normDCAData <- sctransform::vst(as.matrix(dcaMat))
 
 # save the current R session to the file "RSession.Rda"
-save.session("RSession.Rda")
+save.session("sctransform_RSession.Rda")
+# # exit R without saving data
+# q("no")
+# # restart R
+# R
+# # load a saved R session from "RSession.Rda"
+# restore.session("sctransform_RSession.Rda")
 
-# exit R without saving data
-q("no")
-
-# restart R
-R
-
-# load a saved R session from "RSession.Rda"
-restore.session("RSession.Rda")
-
-
-
-
-
-
-
-
-# 2) SCNORM
-
-# # Remove duplicates from the raw file
-# awk '!seen[$1]++' input/publicDS3/12weeks_hippo_publicDS3_dzne_counts_raw_genesymbols.txt > input/publicDS3/12weeks_hippo_publicDS3_dzne_counts_raw_genesymbols_sorted.txt
-output/publicDS3/scanpy/
-
-
-# Run in R
-R 
-library(data.table)
-library(SCnorm)
-
-
-# Create the output directories and files
-jobdir    <- "/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq"
-outputDir <- paste0(jobdir, "/output/manac/01_preprocessed"); system(paste("mkdir -p", outputDir, sep=' '))
-
-rawinfile  <- paste0(jobdir ,"/input/manac/T_cellranger_filtered_manac_counts_raw_genesymbols.txt")
-dcainfile  <- paste0(outputDir,"/02_denoised_cellranger_filtered_manac_counts_raw_genesymbols.txt")
-
-# Get the raw input matrix
-rawMat    <- data.frame(read.table(rawinfile, header=TRUE, sep="\t", na.strings=c(NA, NaN, Inf), row.names=1))
-dcaMat    <- data.frame(read.table(dcainfile, header=TRUE, sep="\t", na.strings=c(NA, NaN, Inf), row.names=1))
-# rawMatDT    <- fread(rawinfile); rawMatDF <- as.data.frame(rawMatDT); rownames(rawMatDT)    <- names(rawMat[0])
-# 		sampleTable$sampleName   <- NULL
-# dcaMatDT    <- fread(dcainfile); 
-
-# Run SCnorm
-rawCond    <- rep(1, dim(rawMat)[1])
-
-countDeptEst <- plotCountDepth(Data = rawMat, Conditions = rawCond, FilterCellProportion = .1, NCores=3)
-normRawMat <- SCnorm(Data=rawMat, K=1, Conditions = rawCond)
-
-
-# conditions  <- rep("1", dim(rawMat)[1])
-conditions <- c()
-for (c in colnames(rawMat)){
-	if(grepl('CA1',c,fixed=TRUE)){
-		conditions <- c(conditions, 'CA1')
-	}else if(grepl('CA23',c,fixed=TRUE)){
-		conditions <- c(conditions, 'CA23')
-	}else if(grepl('DG',c,fixed=TRUE)){
-		conditions <- c(conditions, 'DG')
-	}
-}
-normRawMat  <- SCnorm(Data=rawMat, Conditions=conditions, K=2)
-
-conditions <- c()
-for (c in colnames(dcaMat)){
-	if(grepl('CA1',c,fixed=TRUE)){
-		conditions <- c(conditions, 'CA1')
-	}else if(grepl('CA23',c,fixed=TRUE)){
-		conditions <- c(conditions, 'CA23')
-	}else if(grepl('DG',c,fixed=TRUE)){
-		conditions <- c(conditions, 'DG')
-	}
-}
-normDcaMat  <- SCnorm(Data=dcaMat, Conditions=conditions)
-
-# Get normalized counts matrices
-normRawData <- results(normRawMat, type = "NormalizedData")
-normDcaData <- results(normDcaMat, type = "NormalizedData")
 
 # Save file
-normRawOutfile <- paste(outputDir,"/scnorm_12weeks_hippo_publicDS3_dzne_counts_raw_genesymbols.txt", sep="")
-write.table(normRawData, file = normRawOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
-tnormRawOutfile<- paste(outputDir,"/T_scnorm_12weeks_hippo_publicDS3_dzne_counts_raw_genesymbols.txt", sep="")
-write.table(t(normRawData), file = tnormRawOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
+normRawOutfile <- paste0(outputDir,"/03_cellranger_filtered_manac_counts_normalizedRaw_genesymbols.txt")
+write.table(normRawData$y, file = normRawOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
+tnormRawOutfile<- paste(outputDir,"/03_T_cellranger_filtered_manac_counts_normalizedRaw_genesymbols.txt", sep="")
+write.table(t(normRawData$y), file = tnormRawOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
 
-normDcaOutfile <- paste(outputDir,"/scnorm_denoised_12weeks_hippo_publicDS3_dzne_counts_raw_genesymbols.txt", sep="")
-write.table(normDcaData, file = normDcaOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
-tnormDcaOutfile<- paste(outputDir,"/T_scnorm_denoised_12weeks_hippo_publicDS3_dzne_counts_raw_genesymbols.txt", sep="")
-write.table(t(normDcaData), file = tnormDcaOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
+normDCAOutfile <- paste0(outputDir,"/03_cellranger_filtered_manac_counts_normalizedDCA_genesymbols.txt")
+write.table(normDCAData$y, file = normDCAOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
+tnormDCAOutfile<- paste(outputDir,"/03_T_cellranger_filtered_manac_counts_normalizedDCA_genesymbols.txt", sep="")
+write.table(t(normDCAData$y), file = tnormDCAOutfile, row.names = T, col.names=T, sep = '\t', quote = F)
 
 
 
