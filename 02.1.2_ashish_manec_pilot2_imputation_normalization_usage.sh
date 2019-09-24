fda6afccd5a4e6b87a9528b494000ee64a35cb91
Gaurav Jain <gauravj49@gmail.com>
2019-09-24 17:22:12 +0200

    For pilot 2 data

--- 02.1.2_ashish_manec_pilot2_imputation_normalization_usage.sh
+++ 02.1.2_ashish_manec_pilot2_imputation_normalization_usage.sh
@@ -10,14 +10,14 @@ import scanpy as sc
 from dca.api import dca
 from gjainPyLib import *
 
-projName        = "bulk997_mouse"
+projName        = "stomach1001_mouse"
 output_dir      = "/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq/output/manec/pilot2/01_preprocessed"; create_dir("{0}".format(output_dir))
 minGenesPerCell = 200
 minCellsPergene = 3
 
 # aedata = sc.read_text('input/manec/T_cellranger_filtered_manec_counts_raw_genesymbols.txt')
 adata = sc.read_10x_mtx(
-    '/media/rad/SSD1/temp_manec/bulk997_mouse/outs/filtered_feature_bc_matrix/',  # the directory with the `.mtx` file
+    '/media/rad/SSD1/temp_manec/{0}/outs/filtered_feature_bc_matrix/'.format(projName),  # the directory with the `.mtx` file
     var_names='gene_symbols',                  # use gene symbols for the variable names (variables-axis index)
     cache=True)                                # write a cache file for faster subsequent reading
 adata.var_names_make_unique()
@@ -62,10 +62,10 @@ library(session)
 # Create the output directories and files
 jobdir    <- "/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq"
 outputDir <- paste0(jobdir, "/output/manec/pilot2/01_preprocessed"); system(paste("mkdir -p", outputDir, sep=' '))
-projName  <- "bulk997_mouse"
+projName  <- "bulk1001_mouse"
 
-rawinfile  <- paste0("/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq/output/manec/pilot2/01_preprocessed/01_raw_bulk997_mouse_cellranger_filtered_manec_counts_genesymbols.txt")
-dcainfile  <- paste0("/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq/output/manec/pilot2/01_preprocessed/02_denoised_DCA_bulk997_mouse_cellranger_filtered_manec_counts_genesymbols.txt")
+rawinfile  <- paste0("/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq/output/manec/pilot2/01_preprocessed/01_raw_",projName,"_cellranger_filtered_manec_counts_genesymbols.txt")
+dcainfile  <- paste0("/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq/output/manec/pilot2/01_preprocessed/02_denoised_DCA_",projName,"_cellranger_filtered_manec_counts_genesymbols.txt")
 
 # Get the raw input matrix
 rawMat    <- data.frame(read.table(rawinfile, header=TRUE, sep="\t", na.strings=c(NA, NaN, Inf), row.names=1))
@@ -77,8 +77,8 @@ normRawData <- sctransform::vst(as.matrix(rawMat), return_gene_attr = FALSE)
 normDCAData <- sctransform::vst(as.matrix(dcaMat))
 
 # save the current R session to the file "RSession.Rda"
-sessionfile  <- paste0("/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq/output/manec/pilot2/01_preprocessed/sctransform_bulk997_mouse_RSession.Rda")
-save.session(sessionfile)
+# sessionfile  <- paste0("/home/rad/users/gaurav/projects/seqAnalysis/scrnaseq/output/manec/pilot2/01_preprocessed/sctransform_",projName,"_RSession.Rda")
+# save.session(sessionfile)
 # # exit R without saving data
 # q("no")
 # # restart R
