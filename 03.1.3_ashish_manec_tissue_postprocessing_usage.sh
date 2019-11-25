0a9503dc90f5d72b70ebe7f57e1da985e7307760
Gaurav Jain <gauravj49@gmail.com>
2019-11-25 23:59:10 +0100

    Processing for human Myc, IRES and human CD2 sequences

---
+++ 03.1.3_ashish_manec_tissue_postprocessing_usage.sh
@@ -0,0 +1,34 @@
+# pwd
+cd /home/rad/users/gaurav/projects/seqAnalysis/scrnaseq
+
+projdir="/media/rad/HDD2/temp_manec/bulk1018_mouse_sangerhMYC_IRES/outs"
+cd ${projdir}
+
+samtools view -h ${projdir}/bulk1018_Myc.zeroEditDistance.bam | LC_ALL=C grep -F -f barcodes.tsv | datamash transpose --no-strict -W | grep 'CB' | datamash transpose --no-strict -W | sort -u > ${projdir}/bulk1018_Myc_human_cellIDs.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Get the cell barcodes for every sample containing human myc
+# subsetBamDir="/home/rad/packages/cellranger-3.0.2/subset-bam/subset-bam-1.0-x86_64-linux"
+# ${subsetBamDir}/subset-bam --bam bulk1018_Myc.zeroEditDistance.bam --cell-barcodes filter.txt --out-bam test.bam
+
+
+# samtools view -h ${projdir}/bulk1018_Myc.zeroEditDistance.bam | grep -w CB | cut -f26 | sed 's/CB\:Z\://g' | sort -u > ${projdir}/bulk1018_Myc_human_cellIDs.txt
+
+
