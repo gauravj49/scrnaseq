37d3dc54e0d7eb4f4e0c8d17ceccd6be87844ffb
Gaurav Jain <gauravj49@gmail.com>
2019-12-19 10:08:59 +0100

    New analysis with custom clustering assignment

--- 04.1.5_markusKlegerDuctalDiff_gse84133_ductalSubClusterMitLouvain_usage.sh
+++ 04.1.5_markusKlegerDuctalDiff_gse84133_ductalSubClusterMitLouvain_usage.sh
@@ -304,6 +304,65 @@ plt.savefig("{0}/04_{1}_CustomClusters_UMAP.png".format(qcDir, bname) , bbox_inc
 sc.pl.umap(adataCustom, color=['customClusters'], palette=sc.pl.palettes.vega_10, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, projection='3d', show=False)
 plt.savefig("{0}/04_{1}_CustomClusters_UMAP_3D.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
 
+# 9.1) Marker genes analysis
+sc.tl.rank_genes_groups(adataCustom, groupby='customClusters', key_added='rank_genes_customClusters', n_genes=adata.shape[1])
+
+# adataCustom.obs['customClusters'].value_counts()                                                                                                                           Out[67]: 
+# beta      2362
+# alpha     2100
+# acinar    1126
+# ductal     910
+# delta      607
+# gamma      267
+# Name: customClusters, dtype: int64
+
+# Plot marker genes
+sc.pl.rank_genes_groups(adataCustom, key='rank_genes_customClusters', groups=['beta','alpha','ductal','acinar','delta','gamma'], fontsize=12, show=False)
+plt.savefig("{0}/04_{1}_marker_genes_ranking_customClusters.png".format(qcDir, bname) , bbox_inches='tight'); plt.close('all')
+
+# Save UMAP of marker genes
+sc.pl.umap(adataCustom, color=['customClusters', 'KRT19', 'KRT7', 'CFTR', 'MUC1','PTF1A', 'CPA1', 'AMY2A', 'CTRC', 'PRSS1'], use_raw=False, color_map=mymap, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, show=False)
+plt.savefig("{0}/04_{1}_selected_marker_genes_customClusters_UMAPs.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
+for g in ['customClusters', 'KRT19', 'KRT7', 'CFTR', 'MUC1', 'PTF1A', 'CPA1', 'AMY2A']:
+  sc.pl.umap(adataCustom, color= g, use_raw=False, color_map=mymap, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, show=False)
+  plt.savefig("{0}/04_{1}_marker_gene_{2}_UMAPs.pdf".format(qcDir, bname,g) , bbox_inches='tight'); plt.close('all')
+
+# Save the coordinates of the umap
+rowIdx     = adataCustom.obs.index.values.tolist()
+umapCordDF = pd.DataFrame(adataCustom.obsm['X_umap'],index=rowIdx, columns=['UMAP1','UMAP2', 'UMAP3'])
+umapCordDF.to_csv("{0}/04_normalizedRaw_{1}_customClusters_UMAP_Coordinates.txt".format(countsDir, projName), sep='\t', header=True, index=True, index_label="PatId")
+
+# Dataframe of ranked genes
+for g in ['beta','alpha','ductal','acinar','delta','gamma']:
+  ngDF = pd.DataFrame()
+  for n in ['names', 'scores', 'logfoldchanges',  'pvals', 'pvals_adj']:
+    ngDF[n] = pd.DataFrame(adataCustom.uns['rank_genes_customClusters'][n])[g]
+  # Save dataframes
+  ngDF.to_csv("{0}/04_normalizedRaw_{1}_rank_genes_customClusters_{2}.txt".format(countsDir, projName,g), sep='\t', header=True, index=False, float_format='%.2g')
+
+namesDF    = pd.DataFrame(adataCustom.uns['rank_genes_customClusters']['names'])
+namesDF.to_csv("{0}/04_normalizedRaw_{1}_rank_genes_customClusters_names.txt".format(countsDir, projName), sep='\t', header=True, index=False, float_format='%.2g')
+
+scoresDF   = pd.DataFrame(adataCustom.uns['rank_genes_customClusters']['scores'])
+scoresDF.to_csv("{0}/04_normalizedRaw_{1}_rank_genes_customClusters_scores.txt".format(countsDir, projName), sep='\t', header=True, index=False, float_format='%.2g')
+
+logfcDF    = pd.DataFrame(adataCustom.uns['rank_genes_customClusters']['logfoldchanges'])
+logfcDF.to_csv("{0}/04_normalizedRaw_{1}_rank_genes_customClusters_logfoldchanges.txt".format(countsDir, projName), sep='\t', header=True, index=False, float_format='%.2g')
+
+pvalsDF    = pd.DataFrame(adataCustom.uns['rank_genes_customClusters']['pvals'])
+pvalsDF.to_csv("{0}/04_normalizedRaw_{1}_rank_genes_customClusters_pvals.txt".format(countsDir, projName), sep='\t', header=True, index=False, float_format='%.2g')
+
+pvalsadjDF = pd.DataFrame(adataCustom.uns['rank_genes_customClusters']['pvals_adj'])
+pvalsadjDF.to_csv("{0}/04_normalizedRaw_{1}_rank_genes_customClusters_pvals_adj.txt".format(countsDir, projName), sep='\t', header=True, index=False, float_format='%.2g')
+
+# Save the normalized, log transformed, batch and cell cycle corrected data
+customClustersDF                              = adataCustom.to_df()
+customClustersDF['originalClusterAssignment'] = adataCustom.obs['Cluster']
+customClustersDF['Batch']                     = adataCustom.obs['Batch']
+customClustersDF['louvain_r1']                = adataCustom.obs['louvain_r1']
+customClustersDF['customClusters']            = adataCustom.obs['customClusters']
+customClustersDF.to_csv("{0}/04_normalizedRaw_T_{1}_customClusters.txt".format(countsDir, projName), sep='\t', header=True, index=True, index_label="PatId", float_format='%.2g')
+customClustersDF.T.to_csv("{0}/04_normalizedRaw_{1}_customClusters.txt".format(countsDir, projName), sep='\t', header=True, index=True, index_label="GeneSymbol", float_format='%.2g')
 
 
 
@@ -312,147 +371,6 @@ plt.savefig("{0}/04_{1}_CustomClusters_UMAP_3D.png".format(qcDir, bname) , bbox_
 
 
 
-
-
-
-
-
-
-
-
-
-# Process subcluster 8 individually (but not preprocessing from the beginning)
-# Get the cluster 8 anndata
-adataCluster8 = adata[adata.obs['louvain_r1']=='8',:]
-
-# Mucin and CFTR enriched ductal subpopulation
-sc.pl.umap(adataCluster8, color=['MUC1', 'CFTR', 'TFF1', 'CD44'], use_raw=False, color_map=mymap, size=100, edgecolor='k', linewidth=0.05, alpha=0.9, ncols=2, legend_loc='on data', show=False)
-plt.savefig("{0}/04_{1}_marker_genes_mucin_cftr_enriched_ductal_subpopulation_UMAPs.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-sc.pl.umap(adataCluster8, color=['MUC1', 'CFTR', 'TFF1', 'CD44'], use_raw=False, color_map=mymap, size=100, edgecolor='k', linewidth=0.05, alpha=0.9, ncols=2, legend_loc='on data', projection='3d', show=False)
-plt.savefig("{0}/04_{1}_marker_genes_mucin_cftr_enriched_ductal_subpopulation_3D_UMAPs.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-# Analyze ductal (1077 cells) subgroups (CFTR_high/MUC1_low and CFTR_low/MUC1_high) 
-# Get the index of ductal subcluster
-Cluster8ductalDF = adataCluster8[adataCluster8.obs['Cluster']=='ductal'].to_df()   # 1077
-
-# Get the ratio of expression
-# pd.set_option('display.float_format', lambda x: '%.2f' % x)
-Cluster8ductalDF['CFTR_rank'] = Cluster8ductalDF['CFTR'].rank()
-Cluster8ductalDF['MUC1_rank'] = Cluster8ductalDF['MUC1'].rank()
-Cluster8ductalDF['rank_CFTR_minus_MUC1'] = Cluster8ductalDF.apply(lambda row: (row['CFTR_rank'] - row['MUC1_rank']), axis=1)
-# Cluster8ductalDF[['CFTR', 'MUC1', 'rank_CFTR_minus_MUC1']]
-
-# # Save the relevant data into an excel
-# Cluster8ductalDF[['CFTR', 'MUC1', 'CFTR_rank', 'MUC1_rank', 'rank_CFTR_minus_MUC1']].to_csv("{0}/04_{1}_clean_ductal_subcluster_cluster8_gene_expression_ranks.txt".format(countsDir, projName), sep='\t', header=True, index=True, index_label="CellId", float_format='%.4g')
-
-# Get median for both genes
-# cftrMedianExp = np.float(Cluster8ductalDF['CFTR'].describe()[['50%']].values)
-# muc1MedianExp = np.float(Cluster8ductalDF['MUC1'].describe()[['50%']].values)
-chmlDuctalIdx  = Cluster8ductalDF[Cluster8ductalDF['rank_CFTR_minus_MUC1'] >= 100 ].index.tolist()                # 105
-clmhDuctalIdx  = Cluster8ductalDF[Cluster8ductalDF['rank_CFTR_minus_MUC1'] <= -100].index.tolist()                # 94
-otherDuctalIdx = Cluster8ductalDF.loc[~Cluster8ductalDF.index.isin(chmlDuctalIdx + clmhDuctalIdx)].index.tolist() # 237
-
-# print(len(chmlDuctalIdx))
-# print(len(clmhDuctalIdx))
-# print(len(otherDuctalIdx))
-
-# Get a new subcluster column
-adataCluster8.obs['ductal_subcluster_cluster8'] = adataCluster8.obs['Cluster']
-
-# Add new categories
-adataCluster8.obs['ductal_subcluster_cluster8'].cat.add_categories(['ductal_cftrHigh_muc1Low','ductal_cftrLow_muc1High', 'ductal_other'], inplace=True) 
-
-# Get a new subcluster column
-adataCluster8.obs['ductal_subcluster_cluster8'].loc[chmlDuctalIdx]  = adataCluster8.obs['ductal_subcluster_cluster8'].loc[chmlDuctalIdx].replace('ductal','ductal_cftrHigh_muc1Low') 
-adataCluster8.obs['ductal_subcluster_cluster8'].loc[clmhDuctalIdx]  = adataCluster8.obs['ductal_subcluster_cluster8'].loc[clmhDuctalIdx].replace('ductal','ductal_cftrLow_muc1High') 
-adataCluster8.obs['ductal_subcluster_cluster8'].loc[otherDuctalIdx] = adataCluster8.obs['ductal_subcluster_cluster8'].loc[otherDuctalIdx].replace('ductal','ductal_other') 
-
-adataCleanDucCluster8 = adataCluster8[~((adataCluster8.obs['ductal_subcluster_cluster8'] == 'schwann') | (adataCluster8.obs['ductal_subcluster_cluster8'] == 'acinar') | (adataCluster8.obs['ductal_subcluster_cluster8'] == 'activated_stellate') | (adataCluster8.obs['ductal_subcluster_cluster8'] == 'ductal_other'))]
-
-# Visualize new subclusers
-sc.pl.umap(adataCluster8, color=['ductal_subcluster_cluster8'], palette=sc.pl.palettes.vega_10, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, show=False)
-plt.savefig("{0}/04_norm_{1}_ductal_subcluster_cluster8_UMAP.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-sc.pl.umap(adataCluster8, color=['ductal_subcluster_cluster8'], palette=sc.pl.palettes.vega_10, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, projection='3d', show=False)
-plt.savefig("{0}/04_norm_{1}_ductal_subcluster_cluster8_UMAP_3D.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-sc.pl.umap(adataCleanDucCluster8, color=['ductal_subcluster_cluster8'], palette=sc.pl.palettes.vega_10, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, show=False)
-plt.savefig("{0}/04_norm_{1}_clean_ductal_subcluster_cluster8_UMAP.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-sc.pl.umap(adataCleanDucCluster8, color=['ductal_subcluster_cluster8'], palette=sc.pl.palettes.vega_10, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, projection='3d', show=False)
-plt.savefig("{0}/04_norm_{1}__clean_ductal_subcluster_cluster8_UMAP_UMAP_3D.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-# Mucin and CFTR enriched in clean ductal subpopulation
-sc.pl.umap(adataCleanDucCluster8, color=['MUC1', 'CFTR', 'TFF1', 'CD44'], use_raw=False, color_map=mymap, size=100, edgecolor='k', linewidth=0.05, alpha=0.9, ncols=2, legend_loc='on data', show=False)
-plt.savefig("{0}/04_{1}_marker_genes_mucin_cftr_enriched_clean_ductal_subpopulation_UMAPs.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-sc.pl.umap(adataCleanDucCluster8, color=['MUC1', 'CFTR', 'TFF1', 'CD44'], use_raw=False, color_map=mymap, size=100, edgecolor='k', linewidth=0.05, alpha=0.9, ncols=2, legend_loc='on data', projection='3d', show=False)
-plt.savefig("{0}/04_{1}_marker_genes_mucin_cftr_enriched_clean_ductal_subpopulation_3D_UMAPs.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-
-# 9.1) Marker genes & cluster annotation
-# Calculate marker genes
-sc.tl.rank_genes_groups(adataCleanDucCluster8, groupby='ductal_subcluster_cluster8', groups=['ductal_cftrHigh_muc1Low'], key_added='rank_genes_CftrHighMuc1Low_over_Muc1HighCftrLow', reference='ductal_cftrLow_muc1High', n_genes=adataCleanDucCluster8.shape[1])
-plt.savefig("{0}/04_{1}_marker_genes_ranking_rank_genes_CftrHighMuc1Low_over_Muc1HighCftrLow.png".format(qcDir, bname) , bbox_inches='tight'); plt.close('all')
-
-# Extract and save dataframe of ranked genes
-ngDF = pd.DataFrame()
-for n in ['names', 'scores', 'logfoldchanges',  'pvals', 'pvals_adj']:
-  ngDF[n] = pd.DataFrame(adataCleanDucCluster8.uns['rank_genes_CftrHighMuc1Low_over_Muc1HighCftrLow'][n])['ductal_cftrHigh_muc1Low']
-ngDF.to_csv("{0}/03_normalizedRaw_CleanDuctalSubCluster_rank_genes_CftrHighMuc1Low_over_Muc1HighCftrLow.txt".format(countsDir), sep='\t', header=True, index=False, float_format='%.2g')
-
-# Plot additional marker genes on UMAP
-# Mucin enriched ductal subpopulation
-sc.pl.umap(adataCleanDucCluster8, color=['ductal_subcluster_cluster8', 'MUC1', 'MUC13', 'TFF1', 'TFF2'], use_raw=False, color_map=mymap, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, projection='3d', show=False)
-plt.savefig("{0}/03_{1}_marker_genes_mucin_enriched_ductal_subpopulation_UMAPs_3D.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-# CFTR population
-sc.pl.umap(adataCleanDucCluster8, color=['ductal_subcluster_cluster8', 'KRT19', 'KRT7', 'CFTR', 'AQP3', 'AQP5', 'CA2', 'CA4', 'SCTR', 'SLC26A6'], use_raw=False, color_map=mymap, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, projection='3d', show=False)
-plt.savefig("{0}/03_{1}_marker_genes_cftr_enriched_ductal_subpopulation_UMAPs_3D.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-# CFTR population
-sc.pl.umap(adataCleanDucCluster8, color=['ductal_subcluster_cluster8', 'ANXA2','ANXA3','ANXA4','CDX2','ID2','SOX6','SCTR','CD44','ID3','CLDN4','KLF6','LGALS3','NCOA7','ANXA1','ANXA2','CLDN1','KRT17','LAMC2','S100A16','ABCC3','AMBP','ANXA4','CLDN18','ONECUT2','PDX1','NKX6.1'], use_raw=False, color_map=mymap, size=50, edgecolor='k', linewidth=0.05, alpha=0.9, projection='3d', show=False)
-plt.savefig("{0}/03_{1}_additional_interesting_genes_UMAPs_3D.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-for g in ['ductal_subcluster_cluster8', 'MUC1', 'MUC13', 'TFF1', 'TFF2', 'KRT19', 'KRT7', 'CFTR', 'AQP3', 'AQP5', 'CA2', 'CA4', 'SCTR', 'SLC26A6', 'ANXA2','ANXA3','ANXA4','CDX2','ID2','SOX6','SCTR','CD44','ID3','CLDN4','KLF6','LGALS3','NCOA7','ANXA1','ANXA2','CLDN1','KRT17','LAMC2','S100A16','ABCC3','AMBP','ANXA4','CLDN18','ONECUT2','PDX1','NKX6.1']:
-  sc.pl.umap(adataCleanDucCluster8, color= g, use_raw=False, color_map=mymap, size=50, edgecolor='grey', linewidth=0.01, alpha=0.9, projection='3d', show=False)
-  plt.savefig("{0}/03_{1}_marker_gene_{2}_UMAPs_3D.pdf".format(qcDir, bname,g) , bbox_inches='tight'); plt.close('all')
-
-
-###############################################################################################################################
-# Analysis of Cluster 7 + Cluster 8
-
-# Get the cluster 7+8 anndata
-adataCluster78 = adata[(adata.obs['louvain_r1']=='7')|(adata.obs['louvain_r1']=='8'),:]
-
-# Mucin and CFTR enriched ductal subpopulation
-sc.pl.umap(adataCluster78, color=['MUC1', 'CFTR', 'TFF1', 'CD44'], use_raw=False, color_map=mymap, size=100, edgecolor='k', linewidth=0.05, alpha=0.9, ncols=2, legend_loc='on data', show=False)
-plt.savefig("{0}/04_{1}_marker_genes_mucin_cftr_enriched_ductal_subpopulation_UMAPs.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-sc.pl.umap(adataCluster78, color=['MUC1', 'CFTR', 'TFF1', 'CD44'], use_raw=False, color_map=mymap, size=100, edgecolor='k', linewidth=0.05, alpha=0.9, ncols=2, legend_loc='on data', projection='3d', show=False)
-plt.savefig("{0}/04_{1}_marker_genes_mucin_cftr_enriched_ductal_subpopulation_3D_UMAPs.png".format(qcDir, bname) , bbox_inches='tight', dpi=300); plt.close('all')
-
-
-# Analyze ductal (1077 cells) subgroups (CFTR_high/MUC1_low and CFTR_low/MUC1_high) 
-# Get the index of ductal subcluster
-Cluster8ductalDF = adataCluster8[adataCluster8.obs['Cluster']=='ductal'].to_df()   # 1077
-
-# Get the ratio of expression
-# pd.set_option('display.float_format', lambda x: '%.2f' % x)
-Cluster8ductalDF['CFTR_rank'] = Cluster8ductalDF['CFTR'].rank()
-Cluster8ductalDF['MUC1_rank'] = Cluster8ductalDF['MUC1'].rank()
-Cluster8ductalDF['rank_CFTR_minus_MUC1'] = Cluster8ductalDF.apply(lambda row: (row['CFTR_rank'] - row['MUC1_rank']), axis=1)
-# Cluster8ductalDF[['CFTR', 'MUC1', 'rank_CFTR_minus_MUC1']]
-
-# Get median for both genes
-# cftrMedianExp = np.float(Cluster8ductalDF['CFTR'].describe()[['50%']].values)
-# muc1MedianExp = np.float(Cluster8ductalDF['MUC1'].describe()[['50%']].values)
-chmlDuctalIdx  = Cluster8ductalDF[Cluster8ductalDF['rank_CFTR_minus_MUC1'] >= 100 ].index.tolist()                # 105
-clmhDuctalIdx  = Cluster8ductalDF[Cluster8ductalDF['rank_CFTR_minus_MUC1'] <= -100].index.tolist()                # 94
-otherDuctalIdx = Cluster8ductalDF.loc[~Cluster8ductalDF.index.isin(chmlDuctalIdx + clmhDuctalIdx)].index.tolist() # 237
-
-# print(len(chmlDuctalIdx))
-# print(len(clmhDuctalIdx))
-# print(len(otherDuctalIdx))
-
-# Get a new subcluster column
-adataCluster8.obs['ductal_subcluster_cluster8'] = adataCluster8.obs['Cluster']
 
 #########################################
 # Save session
